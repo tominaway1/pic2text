feb6bc936e681e028a0cb36a90b3b03ffb158368
sandyaaaa <ss4119@columbia.edu>
2013-11-09 10:20:53 -0500

    Revert "Revert "got shit done""
    This reverts commit b039c1a0bac005223e550bf7ece9924907d91dcb.

---
+++ Pic2text/src/com/example/pic2text/ConvertPics.java
@@ -0,0 +1,20 @@
+package com.example.pic2text;
+ 
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.os.Bundle;
+
+ 
+public class ConvertPics extends Activity {
+int TAKE_PHOTO_CODE = 0;
+public static int count=0;
+
+/** Called when the activity is first created. */
+@SuppressLint("NewApi")
+@Override
+public void onCreate(Bundle savedInstanceState){
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.convertpics);
+	}
+}
