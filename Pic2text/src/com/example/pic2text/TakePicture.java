3a308f4a96c17bd2d4067b0c0896027ea7200145
sandyaaaa <ss4119@columbia.edu>
2013-11-09 09:03:21 -0500

    got shit done

---
+++ Pic2text/src/com/example/pic2text/TakePicture.java
@@ -0,0 +1,77 @@
+package com.example.pic2text;
+ 
+import java.io.File;
+import java.io.IOException;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+ 
+public class TakePicture extends Activity {
+int TAKE_PHOTO_CODE = 0;
+public static int count=0;
+
+/** Called when the activity is first created. */
+@SuppressLint("NewApi")
+@Override
+public void onCreate(Bundle savedInstanceState)
+{
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.takepicture);
+
+//here,we are making a folder named picFolder to store pics taken by the camera using this application
+        final String dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/picFolder/"; 
+        File newdir = new File(dir); 
+        newdir.mkdirs();
+    Button convert = (Button) findViewById(R.id.convertF);
+    convert.setOnClickListener(new View.OnClickListener() {
+    	
+    	 @Override
+         public void onClick(View v) {
+             // TODO Auto-generated method stub
+             startActivity(new Intent("com.tutorial.CONVERT"));
+         }
+     });
+    
+    
+    
+    Button capture = (Button) findViewById(R.id.btnCapture);
+    capture.setOnClickListener(new View.OnClickListener() {
+        public void onClick(View v) {
+
+            // here,counter will be incremented each time,and the picture taken by camera will be stored as 1.jpg,2.jpg and likewise.
+            count++;
+            String file = dir+count+".jpg";
+            File newfile = new File(file);
+            try {
+                newfile.createNewFile();
+            } catch (IOException e) {}       
+
+            Uri outputFileUri = Uri.fromFile(newfile);
+
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); 
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+            startActivityForResult(cameraIntent, TAKE_PHOTO_CODE);
+        }
+    });
+}
+
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == TAKE_PHOTO_CODE && resultCode == RESULT_OK) {
+        Log.d("CameraDemo", "Pic saved");
+
+
+    }
+}
+}
